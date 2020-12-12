2955cd15c2aa9929a9fa2329cc2b3ec0137ed2d2
gmn14 <meza.gustavo@pucp.edu.pe>
2020-12-12 16:13:28 -0500

    UAC OOD test 3

--- src/main/java/net/floodlightcontroller/useraccesscontrol/dao/UserDao.java
+++ src/main/java/net/floodlightcontroller/useraccesscontrol/dao/UserDao.java
@@ -3,10 +3,7 @@ package net.floodlightcontroller.useraccesscontrol.dao;
 import net.floodlightcontroller.useraccesscontrol.entity.User;
 import org.slf4j.Logger;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class UserDao extends Dao{
 
@@ -31,22 +28,25 @@ public class UserDao extends Dao{
     public User findUser(Integer code){
         User user = new User();
         String query = "select u.code, u.fullname, u.idrol, u.active, u.active_timestamp, u.ip,\n" +
-                "       u.mac, u.attachment_point from user u where u.code=? limit 1";
+                "       u.mac, u.attachment_point from floodlight.user u where u.code=? limit 1";
 
         try(Connection connection = getConnection();
             PreparedStatement pstmt = connection.prepareStatement(query);
             ) {
-            logger.info("Preparing statement");
+
             pstmt.setInt(1, code);
 
             try(ResultSet rs = pstmt.executeQuery();) {
-                logger.info("Result set got");
                 while (rs.next()){
+                    logger.info("Result 1 : "  + rs.getString(2));
                     user.setCode(rs.getInt(1));
                     user.setFullname(rs.getString(2));
                     user.setIdrol(rs.getInt(3));
                     user.setActive(rs.getBoolean(4));
-                    user.setActive_timestamp(rs.getTimestamp(5).toLocalDateTime());
+                    Timestamp timestamp = rs.getTimestamp(5);
+                    if (timestamp!=null)
+                        user.setActive_timestamp(timestamp.toLocalDateTime());
+                    else user.setActive_timestamp(null);
                     user.setIp(rs.getString(6));
                     user.setMac(rs.getString(7));
                     user.setAttachment_point(rs.getString(8));
