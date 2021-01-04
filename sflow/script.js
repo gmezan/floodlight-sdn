086e507771fb4481fa53d6953fea282239778d41
gmn14 <meza.gustavo@pucp.edu.pe>
2021-01-04 17:07:09 -0500

    debug

--- sflow/script.js
+++ sflow/script.js
@@ -11,70 +11,74 @@ setThreshold('udp_reflection_attack',
  {metric:'udp_reflection',value:100,byFlow:true,timeout:2});
 
 setEventHandler(function(evt) {
- logInfo("Event Handler..");
-	// don't consider inter-switch links
- var link = topologyInterfaceToLink(evt.agent,evt.dataSource);
- if(link) return;
+    logInfo("Event Handler..");
+       // don't consider inter-switch links
+    var link = topologyInterfaceToLink(evt.agent,evt.dataSource);
+    logInfo("link: " + link);
+    if(link) return;
 
- // get port information
- var port = topologyInterfaceToPort(evt.agent,evt.dataSource);
- if(!port) return;
+    // get port information
+    var port = topologyInterfaceToPort(evt.agent,evt.dataSource);
+    logInfo("port: " + port);
+    if(!port) return;
 
- // need OpenFlow info to create Ryu filtering rule
- if(!port.dpid || !port.ofport) return;
+    // need OpenFlow info to create Ryu filtering rule
+    logInfo("port.dpid: " + port.dpid +"\nport.ofport: "+ port.ofport);
+    if(!port.dpid || !port.ofport) return;
 
- // we already have a control for this flow
- if(controls[evt.flowKey]) return;
+    // we already have a control for this flow
+    logInfo(controls[evt.flowKey]);
+    if(controls[evt.flowKey]) return;
 
- var [ipdestination,udpsourceport] = evt.flowKey.split(',');
- 
- var flow = {
-	switch: port.dpid,
-	name: "flow_for_mitigate",
-	priority: "40000",
-	in_port: port.ofport,
-	eth_type:"0x800",
-	ipv4_dst: ipdestination+"/32",
-	ip_proto : "17",
-	active : true
-	
-	//"ip_proto": "0x11",
-	//"ipv4_src": "10.0.0.1",
-	
-	//"active": "true",
-	//"actions": "push_vlan = 0x8100,set-field=eth_vlan_vid->0x09AA, output=1"
- };
- //var msg = {
- // priority:40000,
- // dpid:parseInt(port.dpid,16),
- // match: {
- //  in_port:port.ofport,
-  // dl_type:0x800,
-  // nw_dst:ipdestination+'/32',
-  // nw_proto:17,
- //  tp_src:udpsourceport 
-//  }
- //};
-logInfo(flow.switch);
- var resp = http2({
- //url:'http://'+floodlight+':8080/stats/flowentry/add',
-  url:'http://'+floodlight+':8080/wm/staticflowpusher/json',
-  headers:{'Content-Type':'application/json','Accept':'application/json'},
-  operation:'post',
-  body: JSON.stringify(flow)
-  //body: JSON.stringify(msg)
- });
+    var [ipdestination,udpsourceport] = evt.flowKey.split(',');
 
- controls[evt.flowKey] = {
-  time:Date.now(),
-  threshold:evt.thresholdID,
-  agent:evt.agent,
-  metric:evt.dataSource+'.'+evt.metric,
-  flow:flow
-  //msg:msg
- };
+    var flow = {
+       switch: port.dpid,
+       name: "flow_for_mitigate",
+       priority: "40000",
+       in_port: port.ofport,
+       eth_type:"0x800",
+       ipv4_dst: ipdestination+"/32",
+       ip_proto : "17",
+       active : true
 
- logInfo("blocking " + evt.flowKey);
+       //"ip_proto": "0x11",
+       //"ipv4_src": "10.0.0.1",
+
+       //"active": "true",
+       //"actions": "push_vlan = 0x8100,set-field=eth_vlan_vid->0x09AA, output=1"
+    };
+    //var msg = {
+    // priority:40000,
+    // dpid:parseInt(port.dpid,16),
+    // match: {
+    //  in_port:port.ofport,
+     // dl_type:0x800,
+     // nw_dst:ipdestination+'/32',
+     // nw_proto:17,
+    //  tp_src:udpsourceport
+    //  }
+    //};
+    logInfo(flow.switch);
+    var resp = http2({
+    //url:'http://'+floodlight+':8080/stats/flowentry/add',
+     url:'http://'+floodlight+':8080/wm/staticflowpusher/json',
+     headers:{'Content-Type':'application/json','Accept':'application/json'},
+     operation:'post',
+     body: JSON.stringify(flow)
+     //body: JSON.stringify(msg)
+    });
+
+    controls[evt.flowKey] = {
+     time:Date.now(),
+     threshold:evt.thresholdID,
+     agent:evt.agent,
+     metric:evt.dataSource+'.'+evt.metric,
+     flow:flow
+     //msg:msg
+    };
+
+    logInfo("blocking " + evt.flowKey);
 },['udp_reflection_attack']);
 
 setIntervalHandler(function() {
