7389380c513085cbe4ab7f57bd2e79a87fa69376
gmn14 <meza.gustavo@pucp.edu.pe>
2021-01-04 20:30:46 -0500

    sflow avance

--- sflow/script.js
+++ sflow/script.js
@@ -5,115 +5,126 @@ var controls = {};
 
 logInfo("loggg");
 
-setFlow('udp_reflection',
- {keys:'ipdestination,udpsourceport',value:'frames'});
-setThreshold('udp_reflection_attack',
- {metric:'udp_reflection',value:100,byFlow:true,timeout:2});
+setFlow('udp_reflection', {
+    keys: 'ipdestination,udpsourceport',
+    value: 'frames'
+});
+setThreshold('udp_reflection_attack', {
+    metric: 'udp_reflection',
+    value: 100,
+    byFlow: true,
+    timeout: 2
+});
 
 setEventHandler(function(evt) {
     logInfo("Event Handler..");
-       // don't consider inter-switch links
-    var link = topologyInterfaceToLink(evt.agent,evt.dataSource);
+    // don't consider inter-switch links
+    var link = topologyInterfaceToLink(evt.agent, evt.dataSource);
     logInfo("link: " + link);
-    if(link) return;
+    if (link) return;
 
     // get port information
-    var port = topologyInterfaceToPort(evt.agent,evt.dataSource);
+    var port = topologyInterfaceToPort(evt.agent, evt.dataSource);
     logInfo("port: " + port);
-    if(!port) return;
+    if (!port) return;
 
-   if (!port.dpid.includes(":")) { 
-      var aux = "";
-      var count = 0;
-      for (var i = 0; i < port.dpid.length; i++) {
-         aux += port.dpid.charAt(i);
-         if (count === 1 && (i !== (port.dpid.length-1))) {
-            count = 0;
-            aux += ":";
-         } else {
-            count += 1;
-         }
-      }
-      port.dpid = aux.trim();
-   }
+    // DPID formatting
+    if (!port.dpid.includes(":")) {
+        var aux = "";
+        var count = 0;
+        for (var i = 0; i < port.dpid.length; i++) {
+            aux += port.dpid.charAt(i);
+            if (count === 1 && (i !== (port.dpid.length - 1))) {
+                count = 0;
+                aux += ":";
+            } else {
+                count += 1;
+            }
+        }
+        port.dpid = aux.trim();
+    }
 
     // need OpenFlow info to create Ryu filtering rule
-    logInfo("port.dpid: " + port.dpid +"\nport.ofport: "+ port.ofport);
+    logInfo("port.dpid: " + port.dpid + "\nport.ofport: " + port.ofport);
 
-    if(!port.dpid || !port.ofport) return;
+    if (!port.dpid || !port.ofport) return;
 
     // we already have a control for this flow
     logInfo(controls[evt.flowKey]);
-    if(controls[evt.flowKey]) return;
+    if (controls[evt.flowKey]) return;
 
-    var [ipdestination,udpsourceport] = evt.flowKey.split(',');
+    var [ipdestination, udpsourceport] = evt.flowKey.split(',');
 
     logInfo(flow);
     var flow = {
-       switch: port.dpid,
-       name: "flow_for_mitigate",
-       priority: "40000",
-       in_port: port.ofport,
-       eth_type:"0x800",
-       ipv4_dst: ipdestination+"/32",
-       ip_proto : "17",
-       active : true
+        switch: port.dpid,
+        name: "flow_for_mitigate",
+        priority: "40000",
+        in_port: port.ofport,
+        eth_type: "0x800",
+        ipv4_dst: ipdestination + "/32",
+        ip_proto: "17",
+        active: true
     };
     //var msg = {
     // priority:40000,
     // dpid:parseInt(port.dpid,16),
     // match: {
     //  in_port:port.ofport,
-     // dl_type:0x800,
-     // nw_dst:ipdestination+'/32',
-     // nw_proto:17,
+    // dl_type:0x800,
+    // nw_dst:ipdestination+'/32',
+    // nw_proto:17,
     //  tp_src:udpsourceport
     //  }
     //};
     logInfo(flow.switch);
     var resp = http2({
-    //url:'http://'+floodlight+':8080/stats/flowentry/add',
-     url:'http://'+floodlight+':8080/wm/staticflowpusher/json',
-     headers:{'Content-Type':'application/json','Accept':'application/json'},
-     operation:'post',
-     body: JSON.stringify(flow)
-     //body: JSON.stringify(msg)
+        url: 'http://' + floodlight + ':8080/wm/staticflowpusher/json',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        operation: 'post',
+        body: JSON.stringify(flow)
+        //body: JSON.stringify(msg)
     });
 
     controls[evt.flowKey] = {
-     time:Date.now(),
-     threshold:evt.thresholdID,
-     agent:evt.agent,
-     metric:evt.dataSource+'.'+evt.metric,
-     flow:flow
-     //msg:msg
+        time: Date.now(),
+        threshold: evt.thresholdID,
+        agent: evt.agent,
+        metric: evt.dataSource + '.' + evt.metric,
+        flow: flow
+        //msg:msg
     };
 
     logInfo("blocking " + evt.flowKey);
-},['udp_reflection_attack']);
+}, ['udp_reflection_attack']);
 
 setIntervalHandler(function() {
- logInfo("Interval Handler ..");
-	var now = Date.now();
- for(var key in controls) {
-  let rec = controls[key];
+    logInfo("Interval Handler ..");
+    var now = Date.now();
+    for (var key in controls) {
+        let rec = controls[key];
 
-  // keep control for at least 10 seconds
-  if(now - rec.time < 10000) continue;
-  // keep control if threshold still triggered
-  if(thresholdTriggered(rec.threshold,rec.agent,rec.metric,key)) continue;
+      /*
+        // keep control for at least 10 seconds
+        if (now - rec.time < 10000) continue;
+        // keep control if threshold still triggered
+        if (thresholdTriggered(rec.threshold, rec.agent, rec.metric, key)) continue;
 
-  var resp = http2({
-   //url:'http://'+floodlight+':8080/stats/flowentry/delete',
-   url:'http://'+floodlight+':8080/wm/staticflowpusher/json',
-   headers:{'Content-Type':'application/json','Accept':'application/json'},
-   operation:'delete',
-   //body: JSON.stringify(rec.msg)
-   body: JSON.stringify(rec.flow)
-  });
-
-  delete controls[key];
-
-  logInfo("unblocking " + key);
- }
+        var resp = http2({
+            url: 'http://' + floodlight + ':8080/wm/staticflowpusher/json',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            operation: 'delete',
+            //body: JSON.stringify(rec.msg)
+            body: JSON.stringify(rec.flow)
+        });
+        logInfo("unblocking " + key);
+        */
+        delete controls[key];
+    }
 });
