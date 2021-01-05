8b01bbb1eef5784ba37b8b9c860d27dc22f4c136
gmn14 <meza.gustavo@pucp.edu.pe>
2021-01-04 21:26:24 -0500

    internalsecurity portscanning drop all

--- src/main/java/net/floodlightcontroller/forwarding/Forwarding.java
+++ src/main/java/net/floodlightcontroller/forwarding/Forwarding.java
@@ -113,6 +113,7 @@ public class Forwarding extends ForwardingBase implements IFloodlightModule, IOF
 				return Command.CONTINUE;
             case DROP_ALL:
                 doDropAllFlow(sw, pi, decision, cntx);
+                return Command.CONTINUE;
 			default:
 				log.error("Unexpected decision made for this packet-in={}", pi, decision.getRoutingAction());
 				return Command.CONTINUE;
@@ -157,6 +158,7 @@ public class Forwarding extends ForwardingBase implements IFloodlightModule, IOF
             match.setExact(MatchField.IN_PORT, inPort);
             match.setExact(MatchField.ETH_TYPE, eth.getEtherType());
             if(eth.getEtherType().equals(EthType.IPv4)) {
+                log.info("DROPING ALL IP");
                 IPv4 ip = (IPv4) eth.getPayload();
                 match.setExact(MatchField.IPV4_DST, ip.getDestinationAddress());
                 match.setExact(MatchField.IPV4_SRC, ip.getSourceAddress());
@@ -169,7 +171,7 @@ public class Forwarding extends ForwardingBase implements IFloodlightModule, IOF
             flow.setMatch(match.build());
             flow.setPriority(32767);
 
-            boolean dampened = messageDamper.write(sw, flow.build());
+            boolean dampened = sw.write(flow.build());
             log.debug("OFMessage dampened: {}", dampened);
         } catch (IOException e) {
             log.error("Failure writing drop flow mod", e);
